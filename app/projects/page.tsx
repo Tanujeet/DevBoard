c3441d4c695a0a0f6d4330a589ee14d4f86de032
Tanujeet <singhtanujeet@gmail.com>
2025-07-02 07:49:25 +0530

    Added :Add project component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,19 @@
+import AddProject from "@/components/AddProject";
+
 const page = () => {
   return (
-    <div>page</div>
+    <main>
+      <section>
+        <div className=" max-w-4xl mx-auto p-6 flex justify-between">
+          <h1 className="text-3xl font-bold">Projects</h1>
+          <AddProject />
+        </div>
+        <input
+          placeholder="Search Projects"
+          className="ml-18 border border-black rounded-xl p-2 w-[600px]"
+        />
+      </section>
+    </main>
   );
 };
 
---
+++ components/AddProject.tsx
@@ -0,0 +1,7 @@
+const AddProject = () => {
+  return (
+    <button className="border border-black rounded-2xl p-2">Add Project</button>
+  );
+};
+
+export default AddProject;
