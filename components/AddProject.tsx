ab5eedf0920453befff44e676181ba1db4451377
Tanujeet <singhtanujeet@gmail.com>
2025-07-02 08:27:58 +0530

    Added :Add project

--- components/AddProject.tsx
+++ components/AddProject.tsx
@@ -1,8 +1,43 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+
 const AddProject = () => {
   return (
-    <button className="border border-black rounded-2xl p-2  hover:bg-black hover:text-white transition">
-      Add Project
-    </button>
+    <div>
+      <Dialog>
+        <DialogTrigger className="border border-black rounded-2xl p-2  hover:bg-black hover:text-white transition">
+          Add Project
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>New Project</DialogTitle>
+          </DialogHeader>
+          <div className="space-y-4">
+            <input
+              type="text"
+              placeholder="Enter your project name"
+              className="w-full border border-gray-300 rounded-md px-4 py-2"
+            />
+            <input
+              type="text"
+              placeholder="Description"
+              className="w-full border border-gray-300 rounded-md px-4 py-2"
+            />
+          </div>
+          <div className="flex justify-end gap-2">
+            <Button>Cancel</Button>
+            <Button>Save</Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 };
 
