abeb5da53f72b29072fb31ff90c344ead95e6477
Tanujeet <singhtanujeet@gmail.com>
2025-07-02 07:54:56 +0530

    Added :project frontend

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,17 +1,25 @@
 import AddProject from "@/components/AddProject";
+import { Search } from "lucide-react";
 
 const page = () => {
   return (
     <main>
-      <section>
-        <div className=" max-w-4xl mx-auto p-6 flex justify-between">
+      <section className="max-w-4xl space-y-6 mx-auto p-6 ">
+        <div className=" flex justify-between">
           <h1 className="text-3xl font-bold">Projects</h1>
           <AddProject />
         </div>
-        <input
-          placeholder="Search Projects"
-          className="ml-18 border border-black rounded-xl p-2 w-[600px]"
-        />
+        <div className="relative w-[600px] ">
+          <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <Search className="h-5 w-5 text-gray-400" />
+          </span>
+
+          <input
+            type="text"
+            placeholder="Search Projects"
+            className="w-full border border-black rounded-xl p-2 pl-10 "
+          />
+        </div>
       </section>
     </main>
   );
