6d0e00174ab02ee2710b69acd23c034a29777f9e
Tanujeet <singhtanujeet@gmail.com>
2025-07-04 09:50:02 +0530

    Added : analytics frontend

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,15 +1,29 @@
+"use client";
+
 import { Card, CardContent } from "@/components/ui/card";
+import { axiosInstance } from "@/lib/axios";
+import { useEffect, useState } from "react";
 
 const page = () => {
+  const [stats, setstats] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchAnalytics = async () => {
+      const res = await axiosInstance.get("analytics");
+      setstats(res.data);
+    };
+    fetchAnalytics();
+  }, []);
+
+  if (!stats) return <div>Loading ....</div>;
+
   const cards = [
-    { label: "Total Tasks", value: 120 },
-    { label: "Completed Tasks", value: 85 },
-    { label: "Active Tasks", value: 35 },
-    { label: "Total Projects", value: 15 },
-    { label: "Ongoing Projects", value: 10 },
-    { label: "Archived Projects", value: 5 },
-    { label: "Total Focused Time", value: "25h 30m" },
-    { label: "Pomodoro Sessions", value: 50 },
+    { label: "Total Tasks", value: stats.totalTask },
+    { label: "Completed Tasks", value: stats.completedTask },
+    { label: "Active Tasks", value: stats.activeTask },
+    { label: "Total Projects", value: stats.totalProject },
+    { label: "Ongoing Projects", value: stats.activeProject },
+    { label: "Archived Projects", value: stats.arhcivedProject },
   ];
 
   return (
@@ -22,7 +36,6 @@ const page = () => {
         </p>
       </section>
 
-      {/* Overview Section */}
       <section>
         <h2 className="text-xl font-semibold mb-4">Overview</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
