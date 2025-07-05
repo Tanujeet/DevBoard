527400079f4ff6e74433784f45264269982c3d5b
Tanujeet <singhtanujeet@gmail.com>
2025-07-05 08:41:04 +0530

    Added :completed

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import type { PrismaClient } from "../lib/generated/prisma"; // ✅ type-only import
+const { PrismaClient } = require("../lib/generated/prisma");
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -6,4 +7,6 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
